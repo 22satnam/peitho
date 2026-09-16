95b6334611efaf4e3c6b4a0af5a7523a38f87249
Satnam Singh <95736517+22satnam@users.noreply.github.com>
2026-09-17 03:17:06 +0530

    Show creator signature across Peitho

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,11 @@
 import { createFileRoute } from '@tanstack/react-router'
 import PeithoGate from '../PeithoGate'
+import CreatorSignature from '../components/CreatorSignature'
+
+function PeithoPage() {
+  return <><PeithoGate/><CreatorSignature/></>
+}
 
 export const Route = createFileRoute('/')({
-  component: PeithoGate,
+  component: PeithoPage,
 })
