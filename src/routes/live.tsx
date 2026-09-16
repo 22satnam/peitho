885f646fb0e3914025fe123297d8731aaa249031
Satnam Singh <95736517+22satnam@users.noreply.github.com>
2026-09-17 03:17:13 +0530

    Keep creator signature consistent on live alias

--- src/routes/live.tsx
+++ src/routes/live.tsx
@@ -1,6 +1,11 @@
 import { createFileRoute } from '@tanstack/react-router'
 import PeithoGate from '../PeithoGate'
+import CreatorSignature from '../components/CreatorSignature'
+
+function PeithoLivePage() {
+  return <><PeithoGate/><CreatorSignature/></>
+}
 
 export const Route = createFileRoute('/live')({
-  component: PeithoGate,
+  component: PeithoLivePage,
 })
