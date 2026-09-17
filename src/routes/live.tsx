c724e3685918c389fa4dc62cc9786d4dbdd50f0b
Satnam Singh <95736517+22satnam@users.noreply.github.com>
2026-09-18 01:09:10 +0530

    Apply Peitho product voice on live alias

--- src/routes/live.tsx
+++ src/routes/live.tsx
@@ -3,9 +3,7 @@ import PeithoGate from '../PeithoGate'
 import AuthenticatedApiBridge from '../components/AuthenticatedApiBridge'
 import CreatorSignature from '../components/CreatorSignature'
 import SignedInNav from '../components/SignedInNav'
+import ExperiencePolish from '../components/ExperiencePolish'
 
-function PeithoLivePage() {
-  return <><AuthenticatedApiBridge/><SignedInNav/><PeithoGate/><CreatorSignature/></>
-}
-
-export const Route = createFileRoute('/live')({ component: PeithoLivePage })
+function PeithoLivePage(){return <><AuthenticatedApiBridge/><SignedInNav/><ExperiencePolish/><PeithoGate/><CreatorSignature/></>}
+export const Route=createFileRoute('/live')({component:PeithoLivePage})
