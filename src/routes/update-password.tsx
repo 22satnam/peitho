1dec344408f9d37c7ac2cf283fb398b4cc9ab0ed
Satnam Singh <95736517+22satnam@users.noreply.github.com>
2026-09-18 00:53:25 +0530

    Expose update password page

---
+++ src/routes/update-password.tsx
@@ -0,0 +1,3 @@
+import { createFileRoute } from '@tanstack/react-router'
+import { UpdatePassword } from '../components/PasswordRecovery'
+export const Route=createFileRoute('/update-password')({component:UpdatePassword})
