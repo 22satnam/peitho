b6f8a5838278d0915c663dd70f6e37b9b5f24529
Satnam Singh <95736517+22satnam@users.noreply.github.com>
2026-09-18 01:08:59 +0530

    Apply Peitho product voice to practice flow

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,9 +3,7 @@ import PeithoGate from '../PeithoGate'
 import AuthenticatedApiBridge from '../components/AuthenticatedApiBridge'
 import CreatorSignature from '../components/CreatorSignature'
 import SignedInNav from '../components/SignedInNav'
+import ExperiencePolish from '../components/ExperiencePolish'
 
-function PeithoPage() {
-  return <><AuthenticatedApiBridge/><SignedInNav/><PeithoGate/><CreatorSignature/></>
-}
-
-export const Route = createFileRoute('/')({ component: PeithoPage })
+function PeithoPage(){return <><AuthenticatedApiBridge/><SignedInNav/><ExperiencePolish/><PeithoGate/><CreatorSignature/></>}
+export const Route=createFileRoute('/')({component:PeithoPage})
