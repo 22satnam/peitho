209271a5e5b9a4265bf9919e5665e71e55ade5d3
Satnam Singh <95736517+22satnam@users.noreply.github.com>
2026-09-18 14:12:30 +0530

    Expose individual saved Peitho review route

---
+++ src/routes/history.$sessionId.tsx
@@ -0,0 +1,10 @@
+import {createFileRoute} from '@tanstack/react-router'
+import SessionReview from '../components/SessionReview'
+import SignedInNav from '../components/SignedInNav'
+import CreatorSignature from '../components/CreatorSignature'
+
+function SavedReviewPage(){
+ const{sessionId}=Route.useParams()
+ return <><SignedInNav/><SessionReview sessionId={sessionId}/><CreatorSignature/></>
+}
+export const Route=createFileRoute('/history/$sessionId')({component:SavedReviewPage})
