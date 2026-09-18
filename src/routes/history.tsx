33ed0c91970d15dd5c7d6c7da109852d14fddb1c
Satnam Singh <95736517+22satnam@users.noreply.github.com>
2026-09-18 14:12:53 +0530

    Keep progress in the signed-in Peitho shell

--- src/routes/history.tsx
+++ src/routes/history.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
 import SessionHistory from '../components/SessionHistory'
+import SignedInNav from '../components/SignedInNav'
 import CreatorSignature from '../components/CreatorSignature'
 
-function HistoryPage(){return <><SessionHistory/><CreatorSignature/></>}
+function HistoryPage(){return <><SignedInNav/><SessionHistory/><CreatorSignature/></>}
 export const Route=createFileRoute('/history')({component:HistoryPage})
